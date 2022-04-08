f8b15da843869f8a4a7c0e96f64cc298f4160987
Mimi Majgaard <mimimajgaard@gmail.com>
2022-04-08 15:15:15 -0700

    pushing code

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'; 
+
+function upperCaseCustomerName(){
+     console.log(customerName.toUpperCase())
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+setBestCustomer(bestCustomer)
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const changeLeastFavoriteCustomer = 'Tom';
+
+function changeLeastFavoriteCustomer(){
+    changeLeastFavoriteCustomer = 'Eric' 
+}
+
